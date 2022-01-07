845e1326755f10a842c9b2658552ed59729e29a1
roydukkey <contact@changelog.me>
2022-01-06 19:40:52 -0500

    Replace DocPage properties with frontmatter

--- website/src/theme/FrontMatterContext.ts
+++ website/src/theme/FrontMatterContext.ts
@@ -13,4 +13,8 @@ export default context;
 export interface FrontMatterContext extends FrontMatter {
 	readonly module?: string;
 	readonly link_title?: boolean;
+	/** Additionally, used to identify the field name for <DocPage /> */
+	readonly sidebar_label?: string;
+	/** Additionally, used to identify the field type for <DocPage /> */
+	readonly sidebar_class_name?: string;
 }
