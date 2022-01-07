af919f782fac996acdf03e2d958241e3a24cbc49
roydukkey <contact@changelog.me>
2022-01-07 00:55:44 -0500

    Fix linting and deps

--- website/docs/api/exception/spec/FieldDetails.tsx
+++ website/docs/api/exception/spec/FieldDetails.tsx
@@ -6,4 +6,5 @@ import DocPage from '@site/src/components/DocPage';
 import React from 'react';
 import Spec from './index';
 
+
 export default (): ReturnType<typeof DocPage> => <DocPage spec={Spec} />;
