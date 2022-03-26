77d8b9264830ae5a042000183aac9ec8d98d0aad
roydukkey <contact@changelog.me>
2022-03-25 23:30:27 -0400

    Remove unused front matter

--- website/src/theme/FrontMatter.ts
+++ website/src/theme/FrontMatter.ts
@@ -7,7 +7,6 @@ import type { DocItemFrontMatter } from '@theme/useFrontMatter';
 
 export default interface FrontMatter extends DocItemFrontMatter {
 	readonly module?: string;
-	readonly link_title?: boolean;
 	/** Additionally, used to identify the field name for <DocPage /> */
 	readonly sidebar_label?: string;
 	/** Additionally, used to identify the field type for <DocPage /> */
