b3ab28fe3fa1e74baded3e01a9c30d1ed945009b
roydukkey <contact@changelog.me>
2022-09-11 22:08:04 -0400

    Remove docs site

--- website/docs/api/meta/spec/FieldDetails.tsx
+++
@@ -1,9 +0,0 @@
-// Copyright (c) roydukkey. All rights reserved.
-// Licensed under the MIT. See LICENSE file in the project root for full license information.
-
-import DocPage from '@site/src/components/DocPage';
-import React from 'react';
-import Spec from './index';
-
-
-export default (): ReturnType<typeof DocPage> => <DocPage spec={Spec} />;
--- website/src/mdx.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.mdx' {
-	const src: (props) => JSX.Element;
-	export default src;
-}
