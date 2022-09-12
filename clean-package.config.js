08c3b0cbe4a9fe14e1b460ff65614133a6e99720
roydukkey <contact@changelog.me>
2022-09-11 22:10:04 -0400

    Change from blacklist to whitelist npm packing

--- clean-package.config.js
+++ clean-package.config.js
@@ -12,13 +12,13 @@ const {
 	homepage,
 	keywords: parentKeywords,
 	license,
-	repository
+	repository,
+	files
 } = require('./package.json');
 
 
 // These files should be included from project root in every package.
 const commonFiles = [
-	'.npmignore',
 	'./LICENSE'
 ];
 
@@ -45,6 +45,7 @@ module.exports = {
 		homepage: homepage.replace('#readme', `/tree/master/${repository.directory}$&`),
 		bugs,
 		keywords: [...parentKeywords, ...keywords],
+		files,
 		...package,
 		dependencies,
 		devDependencies,
